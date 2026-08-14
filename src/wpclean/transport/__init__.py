ca63ea01ec36f108edbe5b018d45d945d1e21c09
Lương Văn Duy <97895524+LuongVanDuy@users.noreply.github.com>
2026-08-14 12:28:30 +0700

    Enable zero-byte FTP download fix

--- src/wpclean/transport/__init__.py
+++ src/wpclean/transport/__init__.py
@@ -1,3 +1,8 @@
 from .ftp import FTPConfig, FTPTransport, RemoteFile, TransferFailure, TransferStats
+from .zero_byte_fix import apply_zero_byte_download_fix
+
+
+apply_zero_byte_download_fix()
+
 
 __all__ = ["FTPConfig", "FTPTransport", "RemoteFile", "TransferFailure", "TransferStats"]
